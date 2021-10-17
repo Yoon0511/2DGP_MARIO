53afde01e8f38a180c0561d33858547557d722dd
minwoo2500 <42472602+Yoon0511@users.noreply.github.com>
2021-10-17 23:31:33 +0900

    Revert "camera offset"
    This reverts commit 3b4675dee9fbc271a3539c7a457cd1a6de9e45ee.

--- main.py
+++ main.py
@@ -15,23 +15,11 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-
-
 open_canvas(GAME_WIDTH,GAME_HEIGHT)
 mario = Mario()
 current_time = time.process_time()
 map = MAP()
 map.setup()
-
-def map_offset():
-    mx,my = mario.get_pos()
-    offsetx = GAME_WIDTH/2
-    gap = 0
-    if mx >= offsetx:
-        gap = mx - offsetx
-        mario.set_addpos(-gap,0)
-    for block in map.blocks:
-        block.offet_pos(-gap,0)
 
 while GM.running:
     frame_time = time.process_time() - current_time
@@ -40,7 +28,6 @@ while GM.running:
     clear_canvas()
 
     mario.update(current_time)
-    map_offset()
 
     map.draw()
     mario.draw()
